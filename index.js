c92e48ed25134c0e514c153bfcaa19d767c9319b
Charlotte Currey <charlottecurrey7@gmail.com>
2023-02-01 16:19:39 -0500

    label exercise 1

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+let cookieCounter = 0;
+let cookieParagraph = document.getElementById("cookieParagraph")
+let cookieImage = document.getElementById("cookie1")
+cookieImage.addEventListener("click", function(){
+    cookieCounter++    
+    cookieParagraph.innerHTML = cookieCounter
+})
